8f23d35a4f9a8a39c3ab618a6cb0c0493adf3057
bansikarangiya30 <bansikarangiya27@gmail.com>
2026-07-27 16:08:17 +0530

    Add sitemap

--- temple_app/urls.py
+++ temple_app/urls.py
@@ -1,8 +1,30 @@
+# from django.contrib import admin
+# from django.urls import include, path
+# from temple_app import views
+
+# urlpatterns = [
+#     path('', views.index, name='index'),
+    
+# ]
+
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.contrib.sitemaps.views import sitemap
+
 from temple_app import views
+from temple_app.sitemaps import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+
+    path(
+        'sitemap.xml',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap',
+    ),
 ]
